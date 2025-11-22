c929f9aa457b803e03945d35a5bf70ba2b5e8970
alex <alexmisaellgonzalessoto@users.noreply.github.com>
2025-11-22 12:41:33 +0100

    feat: Creacion de archivo lambda.tf para su integracion

---
+++ infra/terraform/lambda.tf
@@ -0,0 +1,20 @@
+# Lambda
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  # CAMBIAR ESTO POR TU VAR.MYREGION source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename      = "lambda.zip"
+  function_name = "mylambda"
+  role          = aws_iam_role.role.arn
+  handler       = "lambda.lambda_handler"
+  runtime       = "python3.12"
+
+  source_code_hash = filebase64sha256("lambda.zip")
+}
