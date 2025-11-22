c885a544569fbec2b9836d52be09b4e20f302c13
alex <alexmisaellgonzalessoto@users.noreply.github.com>
2025-11-22 18:07:34 +0100

    corrige: No guardé y lo subi sin las actualizaciones

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -1,15 +1,101 @@
-data "aws_iam_policy_document" "lambda_assume_role" {
+data "aws_iam_policy_document" "example" {
   statement {
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
+    sid = "1"
+
+    actions = [
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation",
+    ]
+
+    resources = [
+      "arn:aws:s3:::*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_name}",
+    ]
+
+    condition {
+      test     = "StringLike"
+      variable = "s3:prefix"
+
+      values = [
+        "",
+        "home/",
+        "home/&{aws:username}/",
+      ]
     }
-    actions = ["sts:AssumeRole"]
+  }
+
+  statement {
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_name}/home/&{aws:username}",
+      "arn:aws:s3:::${var.s3_bucket_name}/home/&{aws:username}/*",
+    ]
   }
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name               = "lms-lambda-execution-role"
-  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+resource "aws_iam_policy" "example" {
+  name   = "example_policy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.example.json
+}
+
+#IAM ROLE
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "logs" {
+  role       = aws_iam_role.role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+#POLITICA PARA EVENT BRIGDE
+data "aws_iam_policy_document" "eventbridge_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "events:PutEvents"
+    ]
+    resources = [
+      "arn:aws:events:${var.myregion}:${var.accountId}:event-bus/lms-events-bus"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "lambda_eventbridge" {
+  name   = "lms-lambda-eventbridge-policy"
+  role   = aws_iam_role.lambda_role.id
+  policy = data.aws_iam_policy_document.eventbridge_policy.json
 }
