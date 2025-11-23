f2773009bc1fc4692a3cfebb7aa835bd2f3cd862
alex <alexmisaellgonzalessoto@users.noreply.github.com>
2025-11-23 23:07:18 +0100

    corrige: Politicas y variables actualizadas

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -1,101 +1,195 @@
-data "aws_iam_policy_document" "example" {
+#FUNCIONES LAMBDA Y LAMBDA ROLE
+data "aws_iam_policy_document" "lambda_assume_role" {
   statement {
-    sid = "1"
-
-    actions = [
-      "s3:ListAllMyBuckets",
-      "s3:GetBucketLocation",
-    ]
-
-    resources = [
-      "arn:aws:s3:::*",
-    ]
-  }
-
-  statement {
-    actions = [
-      "s3:ListBucket",
-    ]
-
-    resources = [
-      "arn:aws:s3:::${var.s3_bucket_name}",
-    ]
-
-    condition {
-      test     = "StringLike"
-      variable = "s3:prefix"
-
-      values = [
-        "",
-        "home/",
-        "home/&{aws:username}/",
-      ]
+    sid    = "AllowLambdaAssumeRole"
+    effect = "Allow"
+    
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
     }
-  }
-
-  statement {
-    actions = [
-      "s3:*",
-    ]
-
-    resources = [
-      "arn:aws:s3:::${var.s3_bucket_name}/home/&{aws:username}",
-      "arn:aws:s3:::${var.s3_bucket_name}/home/&{aws:username}/*",
-    ]
+    
+    actions = ["sts:AssumeRole"]
   }
 }
 
-resource "aws_iam_policy" "example" {
-  name   = "example_policy"
-  path   = "/"
-  policy = data.aws_iam_policy_document.example.json
-}
-
-#IAM ROLE
-resource "aws_iam_role" "test_role" {
-  name = "test_role"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
+resource "aws_iam_role" "lms_lambda_role" {
+  name               = "lms-lambda-execution-role-${var.environment}"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
 
   tags = {
-    tag-key = "tag-value"
+    Name        = "lms-lambda-role"
+    Environment = var.environment
+    Service     = "Lambda"
   }
 }
 
-resource "aws_iam_role_policy_attachment" "logs" {
-  role       = aws_iam_role.role.name
+#POLITICA PARA CLOUDWACTH LOGS
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lms_lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-#POLITICA PARA EVENT BRIGDE
-data "aws_iam_policy_document" "eventbridge_policy" {
+#POLITICA PARA EL EVENTBRIDGE
+data "aws_iam_policy_document" "lms_eventbridge_policy" {
   statement {
+    sid    = "AllowPutEventsToLMSBus"
     effect = "Allow"
+    
     actions = [
       "events:PutEvents"
     ]
+    
     resources = [
       "arn:aws:events:${var.myregion}:${var.accountId}:event-bus/lms-events-bus"
     ]
   }
 }
 
-resource "aws_iam_role_policy" "lambda_eventbridge" {
+resource "aws_iam_role_policy" "lms_lambda_eventbridge" {
   name   = "lms-lambda-eventbridge-policy"
-  role   = aws_iam_role.lambda_role.id
-  policy = data.aws_iam_policy_document.eventbridge_policy.json
+  role   = aws_iam_role.lms_lambda_role.id
+  policy = data.aws_iam_policy_document.lms_eventbridge_policy.json
+}
+
+#POLITICA PARA MI QUERIDO S3
+data "aws_iam_policy_document" "lms_s3_policy" {
+  # Listar todos los buckets
+  statement {
+    sid    = "ListAllBuckets"
+    effect = "Allow"
+    
+    actions = [
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation",
+    ]
+    
+    resources = [
+      "arn:aws:s3:::*",
+    ]
+  }
+# Listar contenido del bucket de certificados
+  statement {
+    sid    = "ListCertificatesBucket"
+    effect = "Allow"
+    
+    actions = [
+      "s3:ListBucket",
+    ]
+    
+    resources = [
+      "arn:aws:s3:::${var.lms_certificates_bucket}",
+    ]
+  }
+
+  # Operaciones completas en el bucket de certificados
+  statement {
+    sid    = "ManageCertificates"
+    effect = "Allow"
+    
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+    ]
+    
+    resources = [
+      "arn:aws:s3:::${var.lms_certificates_bucket}/*",
+    ]
+  }
+
+  # Acceso al bucket de recursos educativos (videos, PDFs, etc)
+  statement {
+    sid    = "AccessEducationalResources"
+    effect = "Allow"
+    
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+    
+    resources = [
+      "arn:aws:s3:::${var.lms_resources_bucket}/*",
+    ]
+  }
+
+  # Acceso a carpetas de usuarios (tareas enviadas por estudiantes)
+  statement {
+    sid    = "AccessStudentSubmissions"
+    effect = "Allow"
+    
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+    ]
+    
+    resources = [
+      "arn:aws:s3:::${var.lms_submissions_bucket}/students/*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "lms_s3_policy" {
+  name        = "lms-s3-access-policy-${var.environment}"
+  path        = "/"
+  description = "Política para acceso a buckets S3 del LMS"
+  policy      = data.aws_iam_policy_document.lms_s3_policy.json
+
+  tags = {
+    Name        = "lms-s3-policy"
+    Environment = var.environment
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3" {
+  role       = aws_iam_role.lms_lambda_role.name
+  policy_arn = aws_iam_policy.lms_s3_policy.arn
+}
+
+#POLITICA PARA SNS
+data "aws_iam_policy_document" "lms_sns_policy" {
+  statement {
+    sid    = "AllowPublishToSNS"
+    effect = "Allow"
+    
+    actions = [
+      "sns:Publish",
+    ]
+    
+    resources = [
+      "arn:aws:sns:${var.myregion}:${var.accountId}:lms-notifications-topic"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "lms_lambda_sns" {
+  name   = "lms-lambda-sns-policy"
+  role   = aws_iam_role.lms_lambda_role.id
+  policy = data.aws_iam_policy_document.lms_sns_policy.json
+}
+
+#POLITICA PARA SQS
+data "aws_iam_policy_document" "lms_sqs_policy" {
+  statement {
+    sid    = "AllowSQSOperations"
+    effect = "Allow"
+    
+    actions = [
+      "sqs:SendMessage",
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes",
+    ]
+    
+    resources = [
+      "arn:aws:sqs:${var.myregion}:${var.accountId}:lms-notifications-queue",
+      "arn:aws:sqs:${var.myregion}:${var.accountId}:lms-notifications-dlq",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "lms_lambda_sqs" {
+  name   = "lms-lambda-sqs-policy"
+  role   = aws_iam_role.lms_lambda_role.id
+  policy = data.aws_iam_policy_document.lms_sqs_policy.json
 }
