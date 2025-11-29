f7918b6f2e5df3832390ac82e21b149acd63f2b0
alex <alexmisaellgonzalessoto@users.noreply.github.com>
2025-11-29 01:09:06 +0100

    agrega: Alarmas para el sqs

--- infra/terraform/clw.tf
+++ infra/terraform/clw.tf
@@ -17,3 +17,39 @@ resource "aws_cloudwatch_log_group" "backend_logs" {
     Environment = var.environment
   }
 }
+
+## Alarma para saber que algo falla
+resource "aws_cloudwatch_metric_alarm" "dlq_messages" {
+  alarm_name          = "lms-dlq-messages-${var.environment}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "ApproximateNumberOfMessagesVisible"
+  namespace           = "AWS/SQS"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "1"
+  alarm_description   = "Hay mensajes en la Dead Letter Queue"
+  treat_missing_data  = "notBreaching"
+
+  dimensions = {
+    QueueName = aws_sqs_queue.notifications_dlq.name
+  }
+}
+
+# Alarma para cola de notificaiones llena 
+resource "aws_cloudwatch_metric_alarm" "queue_too_full" {
+  alarm_name          = "lms-queue-backlog-${var.environment}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "ApproximateNumberOfMessagesVisible"
+  namespace           = "AWS/SQS"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "1000"
+  alarm_description   = "La cola de notificaciones tiene más de 1000 mensajes pendientes"
+  treat_missing_data  = "notBreaching"
+
+  dimensions = {
+    QueueName = aws_sqs_queue.notifications.name
+  }
+}
