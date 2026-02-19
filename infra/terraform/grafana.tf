f85126460c3e9edfbd21ee1b77202950ca14b9a4
alex <alexmisaellgonzalessoto@users.noreply.github.com>
2026-02-19 14:32:00 -0500

    terraform: agrega rol IAM y permisos para workspace de Grafana

--- infra/terraform/grafana.tf
+++ infra/terraform/grafana.tf
@@ -1,3 +1,44 @@
+data "aws_iam_policy_document" "grafana_assume_role" {
+  count = var.enable_grafana ? 1 : 0
+
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["grafana.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "grafana_workspace_role" {
+  count = var.enable_grafana ? 1 : 0
+
+  name               = "lms-grafana-workspace-role-${var.environment}"
+  assume_role_policy = data.aws_iam_policy_document.grafana_assume_role[0].json
+
+  tags = {
+    Name        = "lms-grafana-workspace-role"
+    Environment = var.environment
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "grafana_cloudwatch_access" {
+  count = var.enable_grafana ? 1 : 0
+
+  role       = aws_iam_role.grafana_workspace_role[0].name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonGrafanaCloudWatchAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "grafana_xray_access" {
+  count = var.enable_grafana ? 1 : 0
+
+  role       = aws_iam_role.grafana_workspace_role[0].name
+  policy_arn = "arn:aws:iam::aws:policy/AWSXrayReadOnlyAccess"
+}
+
 resource "aws_grafana_workspace" "lms" {
   count = var.enable_grafana ? 1 : 0
 
@@ -5,7 +46,8 @@ resource "aws_grafana_workspace" "lms" {
   description              = "Workspace de Grafana para monitoreo del LMS"
   account_access_type      = "CURRENT_ACCOUNT"
   authentication_providers = var.grafana_authentication_providers
-  permission_type          = "SERVICE_MANAGED"
+  permission_type          = "CUSTOMER_MANAGED"
+  role_arn                 = aws_iam_role.grafana_workspace_role[0].arn
   data_sources             = var.grafana_data_sources
 
   tags = {
